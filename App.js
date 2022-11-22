898964b5c61db871990d0c19ca94caacf2d4bc5e
Eric Wong <107148516+ewbyf@users.noreply.github.com>
2022-11-22 01:20:46 -0600

    Search bar and navbar

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Poggers</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appcontainer}>
+      <View style={styles.topbar}>
+        <Text>Mr. Recipe</Text>
+        <TextInput placeholder='Search for Recipe' style={styles.searchbar}></TextInput>
+      </View>
+      <View>
+
+      </View>
+      <View style={styles.navbar}>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  appcontainer: {
+    height: '100%',
+  },
+  topbar: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    height: '15%',
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#518BFF',
   },
+  searchbar: {
+    width: 300,
+    marginTop: 10,
+    borderWidth: 5,
+    borderColor: 'white',
+    borderRadius: 20,
+    padding: 7,
+    backgroundColor: 'white',
+    color: 'black',
+  },
+  navbar: {
+    position: 'absolute',
+    left: 0,
+    bottom: 0,
+    height: '10%',
+    width: '100%',
+    backgroundColor: '#518BFF',
+  }
 });

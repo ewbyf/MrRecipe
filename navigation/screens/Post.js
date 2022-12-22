b9ee35b36d4a80ebc2a0e99d5a113a03091328cd
ewbyf <107148516+ewbyf@users.noreply.github.com>
2022-12-21 19:17:37 -0600

    Fixed image select

--- navigation/screens/Post.js
+++ navigation/screens/Post.js
@@ -4,12 +4,14 @@ import { firebase } from '../../config';
 import BackArrow from '../../components/BackArrow';
 import { SelectList } from 'react-native-dropdown-select-list'
 import Icon from "react-native-vector-icons/MaterialIcons";
-import ImagePicker from "react-native-image-picker";
+import * as ImagePicker from 'expo-image-picker';
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function Post({ navigation }) {
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
   const [selected, setSelected] = useState("");
+  const [image, setImage] = useState(null);
 
   const data = [
     {key: '1', value: 'Easy'},
@@ -22,6 +24,21 @@ export default function Post({ navigation }) {
     {id: 1, task: 'First ingredient'},
     {id: 2, task: 'Second ingredient'},
   ]);
+
+  // Check if users signed in
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing)
+      setInitializing(false);
+  }
+
+  useEffect(() => {
+  const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (initializing)
+    return null;
 
   const ListItem = ({todo})=> {
     return <View style={styles.listItem}>
@@ -58,55 +75,63 @@ export default function Post({ navigation }) {
     setTodos(newTodos);
   };
 
-  const state = {
-    photo: null,
-  };
-
-  const handleChoosePhoto = () => {
-    const options = {
-      noData: true
-    };
-    ImagePicker.launchImageLibrary(options, response =>{
-      console.log("response", response);
-      if (response.uri) {
-        setState({ photo: response });
-      }
+  const choosePhoto = async() => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [3, 3],
+      quality: 1,
     });
-  };
 
-  function onAuthStateChanged(user) {
-    setUser(user);
-    if (initializing)
-      setInitializing(false);
+    if (!result.canceled) {
+      const source = {uri : result.assets[0].uri};
+      setImage(source);
+    }
   }
 
-  useEffect(() => {
-  const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
-    return subscriber;
-  }, []);
+  const uploadPhoto = async() => {
+    if (image == null)
+      return null;
 
-  if (initializing)
-    return null;
+    const response = await fetch(image.uri);
+    const blob = await response.blob();
+    const filename = image.uri.substring(image.uri.lastIndexOf('/') + 1);
+    var ref = firebase.storage().ref().child(filename).put(blob);
+    try { 
+      await ref;
+      const url = await firebase.storage().ref().child(filename).getDownloadURL();
+      return url;
+    }
+    catch(error) {
+      alert(error.code);
+    }
+  }
 
   if (user) {
-    const {photo} = state; 
     return (
       <View style={styles.appcontainer}>
           <View style={styles.topbar}>
             <BackArrow navigation={navigation}/>
             <Text style={styles.topbarTitle}>Add a Recipe</Text>
           </View>
-          <View styles={styles.items}>
-              {photo && (
-                <Image
-                  source={{uri: photo.uri}}
-                  style={{width: 300, height: 300}}
-                />
-              )}
-              <Button
-                title="Choose Photo"
-                onPress={handleChoosePhoto}
-              />
+          <ScrollView>
+            <View style={{alignItems: 'center'}}>
+              {!image && 
+              <View style={styles.photoSelect}>
+                <Icon name="image" size={75} color={'gray'}/>
+                <Button title="Choose Photo" onPress={choosePhoto}/>
+                <Text style={{color: 'gray', fontSize: 16}}>or</Text>
+                <Button title="Take Photo" onPress={choosePhoto}/>
+              </View>
+              }
+              {image && 
+                <View style={{alignItems: 'center'}}>
+                  <Image source={{uri: image.uri}} style={{width: 300, height: 300, marginTop: 20}}/>
+                  <Button title="Remove Photo" onPress={() => setImage(null)}/>
+                </View>
+              }
+            </View>
+            <View styles={styles.items}>
               <TextInput
                 placeholder="Recipe Name"
                 style={styles.input_container}
@@ -128,27 +153,27 @@ export default function Post({ navigation }) {
                 style={styles.input_container}
               ></TextInput>
 
-             <View styles={styles.ingredients}>
-             <TextInput
-                  placeholder="Add Ingredient" 
-                  value={textInput}
-                  onChangeText={(text)=>setTextInput(text)}
-                  style={styles.input_container}
-              ></TextInput>
-            
-              <TouchableOpacity onPress={addTodo}>
-                  <View style={styles.iconContainer}>
-                    <Icon name="add" color='white' size={30} />
-                  </View>
-              </TouchableOpacity>
-             </View>
+              <View styles={styles.ingredients}>
+              <TextInput
+                    placeholder="Add Ingredient" 
+                    value={textInput}
+                    onChangeText={(text)=>setTextInput(text)}
+                    style={styles.input_container}
+                ></TextInput>
+              
+                <TouchableOpacity onPress={addTodo}>
+                    <View style={styles.iconContainer}>
+                      <Icon name="add" color='white' size={30} />
+                    </View>
+                </TouchableOpacity>
+              </View>
 
-              <FlatList 
+              {/* <FlatList 
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{padding:20, paddingBottom: 100}}
                 data={todos} 
                 renderItem={({item}) => <ListItem todo={item}/>}
-              />
+              /> */}
 
               <SelectList data={data} setSelected={setSelected} dropdownStyles={{backgroundColor: 'white'}}
                   boxStyles={{
@@ -162,7 +187,8 @@ export default function Post({ navigation }) {
               <TouchableOpacity style={styles.button}>
                 <Text style={styles.buttonText}>Publish</Text>
               </TouchableOpacity>
-          </View>
+            </View>
+          </ScrollView>
       </View>
     );
   }
@@ -186,98 +212,86 @@ const styles = StyleSheet.create({
     height: '100%',
     backgroundColor: '#222222',
   },
-    topbar: {
-      paddingTop: 30,
-      height: '13%',
-      width: '100%',
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#518BFF',
-    },
-    topbarTitle: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      color: 'white',
-    },
-    searchbar: {
-      width: 300,
-      borderWidth: 5,
-      marginTop: 15,
-      borderColor: 'white',
-      borderRadius: 20,
-      padding: 7,
-      backgroundColor: 'white',
-      color: 'black',
-    },
-    signinText: {
-      width: 300,
-      borderWidth: 5,
-      marginTop: 15,
-      borderColor: 'white',
-      borderRadius: 20,
-      padding: 7,
-      backgroundColor: '#518BFF',
-      color: 'black',
-      flexDirection: 'row',
-      marginTop: 20
-    },
-    button: {
-      backgroundColor: '#518BFF',
-      width: 175,
-      height: 40,
-      borderRadius: 20,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginTop: 20,
-      marginHorizontal: 102.5,
-    },
-    buttonText: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      color: 'white',
-    },
-    items: {
-      height: '87%',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },  
-    input_container: {
-      borderWidth: 0.5,
-      padding: 12.5,
-      marginHorizontal: 15, 
-      fontSize: 16,
-      marginTop: 20,
-      borderRadius: 50,
-      backgroundColor: 'white'
-    },
-    ingredients: {
-      flexDirection: 'row',
-    },
-    iconContainer: {
-      height: 50,
-      width: 50,
-      backgroundColor: '#518BFF',
-      borderRadius: 25,
-      elevation: 40,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    listItem: {
-      padding: 20,
-      backgroundColor: '#fff',
-      flexDirection: 'row',
-      elevation: 12,
-      borderRadius: 7,
-      marginVertical: 10,
-    },
-    actionIcon: {
-      height: 25,
-      width: 25,
-      backgroundColor: 'red',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginLeft: 5,
-      borderRadius: 3,
-    }
+  topbar: {
+    paddingTop: 30,
+    height: 110,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#518BFF',
+  },
+  topbarTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  items: {
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  photoSelect: {
+    marginVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 300,
+    width: 300,
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderColor: 'white',
+  },
+  input_container: {
+    borderWidth: 0.5,
+    padding: 12.5,
+    marginHorizontal: 15, 
+    fontSize: 16,
+    marginTop: 20,
+    borderRadius: 50,
+    backgroundColor: 'white'
+  },
+  ingredients: {
+    flexDirection: 'row',
+  },
+  iconContainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: '#518BFF',
+    borderRadius: 25,
+    elevation: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listItem: {
+    padding: 20,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    elevation: 12,
+    borderRadius: 7,
+    marginVertical: 10,
+  },
+  actionIcon: {
+    height: 25,
+    width: 25,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 5,
+    borderRadius: 3,
+  },
+  button: {
+    backgroundColor: '#518BFF',
+    width: 175,
+    height: 40,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    marginHorizontal: 102.5,
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
   });
   

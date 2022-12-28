5d7e073895bd670229bc28790b1405511d004f16
ewbyf <107148516+ewbyf@users.noreply.github.com>
2022-12-27 23:25:08 -0600

    favorites system works except heart doesnt update right away

--- navigation/screens/RecipeStack/Recipes.js
+++ navigation/screens/RecipeStack/Recipes.js
@@ -6,6 +6,7 @@ import React from 'react';
 import { Rating } from "react-native-ratings";
 import { firebase } from '../../../config';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { debug } from "react-native-reanimated";
 
 export default function Recipes({ navigation }) {
   const [refreshing, setRefreshing] = useState(false);
@@ -15,8 +16,6 @@ export default function Recipes({ navigation }) {
 
   const [dataList, setDataList] = useState([{key: '1', value: {name: 'a', rating: 'a'}}, {key: '1', value: {name: 'a', rating: 'a'}}]);
   const [recentList, setRecentList] = useState([{key: '1', value: {name: 'a', rating: 'a'}}]);
-  const [favorites, setFavorites] = useState([]);
-
 
   const windowWidth = Dimensions.get('window').width;
   const windowHeight = Dimensions.get('window').height;
@@ -41,7 +40,6 @@ export default function Recipes({ navigation }) {
     let tempList2 = [];
 
     let fav = [];
-    let tempFav = [];
 
     const snapshot = await firebase.firestore().collection('recipes').orderBy('rating', 'desc').get()
     const snapshot2 = await firebase.firestore().collection('recipes').orderBy('timestamp', 'desc').get()
@@ -56,21 +54,25 @@ export default function Recipes({ navigation }) {
     }
 
     await Promise.all(snapshot.docs.map((doc) => {
-      if (fav.indexOf(doc) >= 0) {
-        tempList.push({key: doc.id, value: doc.data(), favorite: 'a'});
+      if (user && fav.indexOf(doc.id) >= 0) {
+        tempList.push({key: doc.id, value: doc.data(), favorite: 'red'});
       }
       else {
-        tempList.push({key: doc.id, value: doc.data(), favorite: 'b'});
+        tempList.push({key: doc.id, value: doc.data(), favorite: 'gray'});
       }
     }))
 
     await Promise.all(snapshot2.docs.map((doc) => {
-      tempList2.push({key: doc.id, value: doc.data()});
+      if (user && fav.indexOf(doc.id) >= 0) {
+        tempList2.push({key: doc.id, value: doc.data(), favorite: 'red'});
+      }
+      else {
+        tempList2.push({key: doc.id, value: doc.data(), favorite: 'gray'});
+      }
     }))
 
     setDataList(tempList);
     setRecentList(tempList2);
-    setFavorites(tempFav);
   }
 
   useEffect(() => {
@@ -84,40 +86,45 @@ export default function Recipes({ navigation }) {
     wait(800).then(() => setRefreshing(false));
   }, []);
 
-  const checkFavorite = async(doc) => {
-    return 'red';
-    if (user) {
-      await firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).get()
-      .then((snap) => {
-        return true;
-        // console.debug(snap.data().favorites.includes(doc))
-        // return (snap.data().favorites.includes(doc));
-      })
-      .catch((error) => {
-        alert(error.message);
-      })
-    }
-  }
 
-  const favorite = async(doc) => {
+  const favorite = async(doc, list) => {
     if (user) {
+      let fav = [];
       let temp = [];
+      let index = -1;
+      if (list == "trending") {
+        temp = dataList;
+        index = dataList.findIndex(item => item.key == doc);
+      }
+      else {
+        temp = recentList;
+        index = recentList.findIndex(item => item.key == doc);
+      }
+
       await firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).get()
       .then((snap) => {
-        temp = snap.data().favorites;
-        if (temp.indexOf(doc) != -1) {
-          temp.splice(snap.data().favorites.indexOf(doc), 1);
-          firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).update({favorites: temp});
+        fav = snap.data().favorites;
+        if (fav.indexOf(doc) != -1) {
+          fav.splice(snap.data().favorites.indexOf(doc), 1);
+          firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).update({favorites: fav});
+          temp[index].favorite = 'gray';
         }
         else {
-          temp.push(doc);
-          firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).update({favorites: temp});
-          setFavorites([...favorites, 1]);
+          fav.push(doc);
+          firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid).update({favorites: fav});
+          temp[index].favorite = 'red';
         }
       })
       .catch((error) => {
         alert(error.message);
       })
+
+      if (list == "trending") {
+        setDataList(temp);
+      }
+      else {
+        setRecentList(temp);
+      }
     }
     else {
       Alert.alert("Not Signed In", "You msut be signed in to favorite a recipe.");
@@ -129,134 +136,136 @@ export default function Recipes({ navigation }) {
   }
 
   return (
-      <View style={global.appContainer}>
-          <View style={global.searchTopbar}>
-              <Text style={global.topbarTitle}>Mr. Recipe</Text>
-              <TextInput placeholder='Search for Recipe' style={global.searchbar}></TextInput>
-          </View>
-          <ScrollView style={{height: windowHeight, width: windowWidth}} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
-            <View style={{marginBottom: 300, width: windowWidth, height: windowHeight}}>
-              <Text style={styles.titleText}>Featured Recipe</Text>
-              <View style={styles.featuredContainer}>
-                <FlashList
-                  data={dataList.slice(0, 1)}
-                  renderItem={({item}) => (
-                    <TouchableOpacity style={{width: windowWidth-40, height: 300, borderRadius: 10}} onPress={() => navigation.navigate("DishScreen", {doc: item.key})}>
-                      <View style={styles.list}>
-                        <Image source={{uri: (item.value.image ? item.value.image : 'https://imgur.com/hNwMcZQ.png')}} style={styles.featuredImage}/>
-                        <View style={{flexDirection: 'row', width: '100%'}}>
-                          <View style={{flex: 1}}>
-                            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>{item.value.name}</Text>
-                            <Text style={{color: 'gray'}}>{item.value.difficulty}</Text>
-                            <Text style={{color: 'gray'}}>{((item.value.cooktime + item.value.preptime) / 60).toFixed(1)}+ hrs</Text>
-                          </View>
-                          <View style={styles.ratingContainer}>
-                            <Rating
-                              style={styles.ratingBar}
-                              ratingCount={5}
-                              imageSize={16}
-                              readonly={true}
-                              type={'custom'}
-                              ratingBackgroundColor={'gray'}
-                              tintColor={'#282828'}
-                              startingValue={item.value.rating}
-                            />
-                            <Text style={styles.rating}>{item.value.rating} of 5</Text>
-                            <Icon name='heart' color={'gray'} size={20} />
-                          </View>
+    <View style={global.appContainer}>
+        <View style={global.searchTopbar}>
+            <Text style={global.topbarTitle}>Mr. Recipe</Text>
+            <TextInput placeholder='Search for Recipe' style={global.searchbar}></TextInput>
+        </View>
+        <ScrollView style={{height: windowHeight, width: windowWidth}} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
+          <View style={{marginBottom: 300, width: windowWidth, height: windowHeight}}>
+            <Text style={styles.titleText}>Featured Recipe</Text>
+            <View style={styles.featuredContainer}>
+              <FlashList
+                data={dataList.slice(0, 1)}
+                renderItem={({item}) => (
+                  <TouchableOpacity style={{width: windowWidth-40, height: 300, borderRadius: 10}} onPress={() => navigation.navigate("DishScreen", {doc: item.key})}>
+                    <View style={styles.list}>
+                      <Image source={{uri: (item.value.image ? item.value.image : 'https://imgur.com/hNwMcZQ.png')}} style={styles.featuredImage}/>
+                      <View style={{flexDirection: 'row', width: '100%'}}>
+                        <View style={{flex: 1}}>
+                          <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>{item.value.name}</Text>
+                          <Text style={{color: 'gray'}}>{item.value.difficulty}</Text>
+                          <Text style={{color: 'gray'}}>{((item.value.cooktime + item.value.preptime) / 60).toFixed(1)}+ hrs</Text>
+                        </View>
+                        <View style={styles.ratingContainer}>
+                          <Rating
+                            style={styles.ratingBar}
+                            ratingCount={5}
+                            imageSize={16}
+                            readonly={true}
+                            type={'custom'}
+                            ratingBackgroundColor={'gray'}
+                            tintColor={'#282828'}
+                            startingValue={item.value.rating}
+                          />
+                          <Text style={styles.rating}>{item.value.rating} of 5</Text>
+                          <TouchableOpacity onPress={() => favorite(item.key, "trending")}>
+                            <Icon name='heart' color={item.favorite} size={20} />
+                          </TouchableOpacity>
+                          
                         </View>
                       </View>
-                    </TouchableOpacity>
-                  )}
-                  estimatedItemSize={1}
-                />
-              </View>
-              <Text style={styles.titleText}>Trending</Text>
-              <View style={styles.trendingContainer}>
-                <FlashList
-                  data={dataList.slice(1)}
-                  renderItem={({item}) => (
-                    <TouchableOpacity style={{width: windowWidth/1.5 - 20, height: 250, borderRadius: 10}} onPress={() => navigation.navigate("DishScreen", {doc: item.key})}>
-                      <View style={[styles.list, {marginHorizontal: 10, height: 250}]}>
-                        <Image source={{uri: (item.value.image ? item.value.image : 'https://imgur.com/hNwMcZQ.png')}} style={styles.smallImage}/>
-                        <View style={{width: '100%', height: 85}}>
-                          <View style={{}}>
-                            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>{item.value.name}</Text>
-                            <Text style={{color: 'gray'}}>{item.value.difficulty}</Text>
-                            <Text style={{color: 'gray'}}>{((item.value.cooktime + item.value.preptime) / 60).toFixed(1)}+ hrs</Text>
-                          </View>
-                          <View style={{flexDirection: 'row', marginTop: 'auto', alignItems: 'center'}}>
-                            <Rating
-                              ratingCount={5}
-                              imageSize={16}
-                              readonly={true}
-                              type={'custom'}
-                              ratingBackgroundColor={'gray'}
-                              tintColor={'#282828'}
-                              startingValue={item.value.rating}
-                            />
-                            <Text style={styles.rating}>{item.value.rating} of 5</Text>
-                            <Text>{item.favorite}</Text>
-                            
-                          </View>
-                        </View>
-                        <TouchableOpacity style={{position: 'absolute', bottom: 6, right: 20}} onPress={() => favorite(item.key)}>
-                          <Icon name='heart' color={item.favorite == true ? 'red' : 'gray'} size={20}/>
-                        </TouchableOpacity>
-                      </View>
-                    </TouchableOpacity>
-                  )}
-                  estimatedItemSize={10}
-                  numColumns={1}
-                  showsHorizontalScrollIndicator={false}
-                  horizontal
-                />
-              </View>
-              <Text style={styles.titleText}>Recent</Text>
-              <View style={styles.trendingContainer}>
-                <FlashList
-                  data={recentList}
-                  renderItem={({item}) => (
-                    <TouchableOpacity style={{width: windowWidth/1.5 - 20, height: 250, borderRadius: 10}} onPress={() => navigation.navigate("DishScreen", {doc: item.key})}>
-                      <View style={[styles.list, {marginHorizontal: 10, height: 250}]}>
-                        <Image source={{uri: (item.value.image ? item.value.image : 'https://imgur.com/hNwMcZQ.png')}} style={styles.smallImage}/>
-                        <View style={{width: '100%', height: 85}}>
-                          <View style={{}}>
-                            <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>{item.value.name}</Text>
-                            <Text style={{color: 'gray'}}>{item.value.difficulty}</Text>
-                            <Text style={{color: 'gray'}}>{((item.value.cooktime + item.value.preptime) / 60).toFixed(1)}+ hrs</Text>
-                          </View>
-                          <View style={{flexDirection: 'row', marginTop: 'auto', alignItems: 'center'}}>
-                            <Rating
-                              ratingCount={5}
-                              imageSize={16}
-                              readonly={true}
-                              type={'custom'}
-                              ratingBackgroundColor={'gray'}
-                              tintColor={'#282828'}
-                              startingValue={item.value.rating}
-                            />
-                            <Text style={styles.rating}>{item.value.rating} of 5</Text>
-                            
-                          </View>
-                        </View>
-                        <TouchableOpacity style={{position: 'absolute', bottom: 6, right: 20}} onPress={() => favorite(item.key)}>
-                          <Icon name='heart' color={checkFavorite(item.key)} size={20}/>
-                        </TouchableOpacity>
-                      </View>
-                    </TouchableOpacity>
-                  )}
-                  estimatedItemSize={10}
-                  numColumns={1}
-                  showsHorizontalScrollIndicator={false}
-                  horizontal
-                />
-              </View>
-              <View>
-              </View>
+                    </View>
+                  </TouchableOpacity>
+                )}
+                estimatedItemSize={1}
+              />
             </View>
-          </ScrollView>
-      </View>
+            <Text style={styles.titleText}>Trending</Text>
+            <View style={styles.trendingContainer}>
+              <FlashList
+                data={dataList.slice(1)}
+                extraData={dataList}
+                renderItem={({item}) => (
+                  <TouchableOpacity style={{width: windowWidth/1.5 - 20, height: 250, borderRadius: 10}} onPress={() => navigation.navigate("DishScreen", {doc: item.key})}>
+                    <View style={[styles.list, {marginHorizontal: 10, height: 250}]}>
+                      <Image source={{uri: (item.value.image ? item.value.image : 'https://imgur.com/hNwMcZQ.png')}} style={styles.smallImage}/>
+                      <View style={{width: '100%', height: 85}}>
+                        <View style={{}}>
+                          <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>{item.value.name}</Text>
+                          <Text style={{color: 'gray'}}>{item.value.difficulty}</Text>
+                          <Text style={{color: 'gray'}}>{((item.value.cooktime + item.value.preptime) / 60).toFixed(1)}+ hrs</Text>
+                        </View>
+                        <View style={{flexDirection: 'row', marginTop: 'auto', alignItems: 'center'}}>
+                          <Rating
+                            ratingCount={5}
+                            imageSize={16}
+                            readonly={true}
+                            type={'custom'}
+                            ratingBackgroundColor={'gray'}
+                            tintColor={'#282828'}
+                            startingValue={item.value.rating}
+                          />
+                          <Text style={styles.rating}>{item.value.rating} of 5</Text>   
+                        </View>
+                      </View>
+                      <TouchableOpacity style={{position: 'absolute', bottom: 6, right: 20}} onPress={() => favorite(item.key, "trending")}>
+                        <Icon name='heart' color={item.favorite} size={20}/>
+                      </TouchableOpacity>
+                    </View>
+                  </TouchableOpacity>
+                )}
+                estimatedItemSize={10}
+                numColumns={1}
+                showsHorizontalScrollIndicator={false}
+                horizontal
+              />
+            </View>
+            <Text style={styles.titleText}>Recent</Text>
+            <View style={styles.trendingContainer}>
+              <FlashList
+                data={recentList}
+                renderItem={({item}) => (
+                  <TouchableOpacity style={{width: windowWidth/1.5 - 20, height: 250, borderRadius: 10}} onPress={() => navigation.navigate("DishScreen", {doc: item.key})}>
+                    <View style={[styles.list, {marginHorizontal: 10, height: 250}]}>
+                      <Image source={{uri: (item.value.image ? item.value.image : 'https://imgur.com/hNwMcZQ.png')}} style={styles.smallImage}/>
+                      <View style={{width: '100%', height: 85}}>
+                        <View style={{}}>
+                          <Text style={{color: 'white', fontWeight: 'bold', fontSize: 16}}>{item.value.name}</Text>
+                          <Text style={{color: 'gray'}}>{item.value.difficulty}</Text>
+                          <Text style={{color: 'gray'}}>{((item.value.cooktime + item.value.preptime) / 60).toFixed(1)}+ hrs</Text>
+                        </View>
+                        <View style={{flexDirection: 'row', marginTop: 'auto', alignItems: 'center'}}>
+                          <Rating
+                            ratingCount={5}
+                            imageSize={16}
+                            readonly={true}
+                            type={'custom'}
+                            ratingBackgroundColor={'gray'}
+                            tintColor={'#282828'}
+                            startingValue={item.value.rating}
+                          />
+                          <Text style={styles.rating}>{item.value.rating} of 5</Text>
+                          
+                        </View>
+                      </View>
+                      <TouchableOpacity style={{position: 'absolute', bottom: 6, right: 20}} onPress={() => favorite(item.key, "recent")}>
+                        <Icon name='heart' color={item.favorite} size={20}/>
+                      </TouchableOpacity>
+                    </View>
+                  </TouchableOpacity>
+                )}
+                estimatedItemSize={10}
+                numColumns={1}
+                showsHorizontalScrollIndicator={false}
+                horizontal
+              />
+            </View>
+            <View>
+            </View>
+          </View>
+        </ScrollView>
+    </View>
   );
 }
 
